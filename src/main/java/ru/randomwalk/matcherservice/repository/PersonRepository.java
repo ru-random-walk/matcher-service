c203498849312a6ccbf6546db553f8600f99e736
Nikita Kuritsyn <martyz89@yandex.ru>
2024-12-09 21:40:10 +0300

    add ANY_MATCH and NO_FILTER search

--- src/main/java/ru/randomwalk/matcherservice/repository/PersonRepository.java
+++ src/main/java/ru/randomwalk/matcherservice/repository/PersonRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
-import ru.randomwalk.matcherservice.model.enam.AppointmentStatus;
 import ru.randomwalk.matcherservice.model.entity.Person;
 
 import java.util.List;
@@ -26,24 +25,27 @@ public interface PersonRepository extends JpaRepository<Person, UUID> {
     @Query("""
         select p from Person as p
         inner join p.location as l
-        inner join p.clubs as cl
+        join fetch p.clubs as cl
         where within(l.position, :point, :distanceInMeters) = true
         and p.inSearch = true
         and p.id != :excludePersonId
-        and ((p.groupFilterType = 'ANY_MATCH' and cl.inFilter = true and cl.clubId in :groupIdsInFilter)
-        or (p.groupFilterType = 'NO_FILTER' and cl.clubId in :groupIdsInFilter))
+        and (
+                (p.groupFilterType = 'ANY_MATCH' and cl.inFilter = true and cl.clubId in :groupIdsInFilter)
+                or (p.groupFilterType = 'NO_FILTER' and (:isAnyMatchSearch = false or cl.clubId in :groupIdsInFilter))
+                or (p.groupFilterType = 'ALL_MATCH' and cl.clubId in :groupIdsInFilter)
+            )
+        order by p.inSearchFrom
     """
     )
     @QueryHints(
             @QueryHint(name = AvailableHints.HINT_FETCH_SIZE, value = "20")
     )
-    //TODO: Не забыть, что можем подобрать с фильтром ALL_MATCH, нужно дополнительно проверять на соответсвие в коде
-    Stream<Person> findByDistanceAndAnyGroupIdsInFilter(
+    Stream<Person> findByDistanceAndGroupIdsInFilterByFilterType(
             @Param("excludePersonId") UUID excludePersonId,
-            @Param("appointmentStatus") AppointmentStatus appointmentStatus,
             @Param("point") Point point,
             @Param("distanceInMeters") Double distanceInMeters,
-            @Param("groupIdsInFilter") @Nullable List<UUID> groupIds
+            @Param("groupIdsInFilter") @Nullable List<UUID> groupIds,
+            @Param("isAnyMatchSearch") boolean isAnyMatchSearch
     );
 
     @EntityGraph(type = EntityGraph.EntityGraphType.LOAD, attributePaths = {"availableTimes", "dayLimit"})
@@ -52,10 +54,10 @@ public interface PersonRepository extends JpaRepository<Person, UUID> {
             JOIN person_club pc ON p.id = pc.person_id
             JOIN location l ON p.location_id = l.id
         WHERE p.id != :excludePersonId
-        AND p.in_search = true
-        AND pc.in_filter = true
-        AND ST_DWithin(l.position, :point, :distanceInMeters) = true
-        AND pc.club_id IN :groupIdsInFilter
+            AND p.in_search = true
+            AND pc.in_filter = true
+            AND ST_DWithin(l.position, :point, :distanceInMeters) = true
+            AND pc.club_id IN :groupIdsInFilter
         GROUP BY p.id
         HAVING COUNT(DISTINCT pc.club_id) = :filterGroupCount
         ORDER BY p.in_search_from
--- src/main/java/ru/randomwalk/matcherservice/service/impl/PersonServiceImpl.java
+++ src/main/java/ru/randomwalk/matcherservice/service/impl/PersonServiceImpl.java
@@ -7,15 +7,18 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.randomwalk.matcherservice.model.entity.Club;
 import ru.randomwalk.matcherservice.model.entity.Person;
-import ru.randomwalk.matcherservice.model.exception.MatcherBadRequestException;
 import ru.randomwalk.matcherservice.model.exception.MatcherNotFoundException;
 import ru.randomwalk.matcherservice.repository.PersonRepository;
 import ru.randomwalk.matcherservice.service.PersonService;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static ru.randomwalk.matcherservice.model.enam.FilterType.ALL_MATCH;
 
 @Service
 @RequiredArgsConstructor
@@ -35,12 +38,13 @@ public class PersonServiceImpl implements PersonService {
     public Stream<Person> streamSuitableCandidatesForPerson(Person person) {
         log.info("Searching for suitable candidates for {}", person.getId());
         return switch (person.getGroupFilterType()) {
-            case ALL_MATCH -> getAllMatchPersonStream(person);
-            default -> throw new MatcherBadRequestException("Unsupported filter type");
+            case ALL_MATCH -> getAllMatchPersonStreamForPerson(person);
+            case ANY_MATCH -> getAnyMatchPersonStreamForPerson(person);
+            case NO_FILTER -> getWithoutFilterPersonStreamForPerson(person);
         };
     }
 
-    private Stream<Person> getAllMatchPersonStream(Person person) {
+    private Stream<Person> getAllMatchPersonStreamForPerson(Person person) {
         List<UUID> clubsInFilterId = getClubsInFilterIds(person);
 
         log.info("Searching for partners for person {} with clubs that are all matching to: {}", person.getId(), clubsInFilterId);
@@ -52,6 +56,46 @@ public class PersonServiceImpl implements PersonService {
                 clubsInFilterId,
                 clubsInFilterId.size()
         );
+    }
+
+    private Stream<Person> getAnyMatchPersonStreamForPerson(Person person) {
+        List<UUID> clubsInFilterId = getClubsInFilterIds(person);
+        Set<UUID> clubsIdsSet = new HashSet<>(clubsInFilterId);
+
+        log.info("Searching for partners for person {} with any club from list: {}", person.getId(), clubsInFilterId);
+
+        return personRepository.findByDistanceAndGroupIdsInFilterByFilterType(
+                person.getId(),
+                person.getLocation().getPosition(),
+                Double.valueOf(person.getSearchAreaInMeters()),
+                clubsInFilterId,
+                true
+        ).filter(candidate -> filterAllGroupsMatchIfNeeded(candidate, clubsIdsSet));
+    }
+
+    private Stream<Person> getWithoutFilterPersonStreamForPerson(Person person) {
+        List<UUID> clubsInFilterId = getClubsInFilterIds(person);
+        Set<UUID> clubsIdsSet = new HashSet<>(clubsInFilterId);
+
+        log.info("Searching for partners for person {} with no filter", person.getId());
+
+        return personRepository.findByDistanceAndGroupIdsInFilterByFilterType(
+                person.getId(),
+                person.getLocation().getPosition(),
+                Double.valueOf(person.getSearchAreaInMeters()),
+                clubsInFilterId,
+                false
+        ).filter(candidate -> filterAllGroupsMatchIfNeeded(candidate, clubsIdsSet));
+    }
+
+    private boolean filterAllGroupsMatchIfNeeded(Person candidate, Set<UUID> clubsInFilterId) {
+        if (candidate.getGroupFilterType() != ALL_MATCH) {
+            return true;
+        }
+
+        return candidate.getClubs().stream()
+                .map(Club::getClubId)
+                .allMatch(clubsInFilterId::contains);
     }
 
     @Override
