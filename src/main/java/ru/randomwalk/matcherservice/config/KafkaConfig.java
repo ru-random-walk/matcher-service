8b7281579afc559c9356a662a287d11719e60ddd
Nikita Kuritsyn <martyz89@yandex.ru>
2025-01-03 17:10:51 +0300

    RANDOMWALK-97 Listen for new users from registration.user

---
+++ src/main/java/ru/randomwalk/matcherservice/config/KafkaConfig.java
@@ -0,0 +1,42 @@
+package ru.randomwalk.matcherservice.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(KafkaProperties kafkaProperties) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerConfig(kafkaProperties));
+        return factory;
+    }
+
+    public ConsumerFactory<String, String> consumerConfig(KafkaProperties kafkaConfig) {
+        Map<String, Object> properties = new HashMap<>();
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfig.getConsumer().getGroupId());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+}
---
+++ src/main/java/ru/randomwalk/matcherservice/listener/UserRegistrationListener.java
@@ -0,0 +1,34 @@
+package ru.randomwalk.matcherservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import ru.random.walk.dto.RegisteredUserInfoEvent;
+import ru.random.walk.kafka.EventTopic;
+import ru.randomwalk.matcherservice.service.PersonService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationListener {
+
+    private final ObjectMapper objectMapper;
+    private final PersonService personService;
+
+    @KafkaListener(topics = EventTopic.USER_REGISTRATION)
+    public void process(String message) {
+        try {
+            log.info("Processing message {} from topic {}", message, EventTopic.USER_REGISTRATION);
+
+            RegisteredUserInfoEvent userInfoEvent = objectMapper.readValue(message, RegisteredUserInfoEvent.class);
+            personService.addNewPerson(userInfoEvent);
+
+            log.info("Message {} is processed", message);
+        } catch (Exception e) {
+            log.error("Error processing message {} from topic {}", message, EventTopic.USER_REGISTRATION, e);
+            throw new RuntimeException(e);
+        }
+    }
+}
